d460a4fe60fa850adf013b418b2aefb069b19ae7
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-05 16:56:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 numbers = [1,2,3]
 def square_array(array)
   count = 0
-  array = [81**2, 100**2, 256**2, 625**2]
+  array = [9**2, 10**2, 16**2, 25**2]
   while count < array.length do
     array[count]
     count += 1
