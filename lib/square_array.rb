97d9240989b7d9b558b6dca746050ff1ac221fd1
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-05 17:36:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,1 @@
-numbers = [1,2,3]
-def square_array(array)
-  counter = 0
-  new_array = []
-  while count < array.length do
-    
-    count += 1
-  end
-  array
-end
-
-square_array(numbers)
+array = [index**2, index**2, index**2, index**2]
